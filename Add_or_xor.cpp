08f40e6c392eee417d40468d9bae79d828ed3ca2
sanikakhanduja <138996276+sanikakhanduja@users.noreply.github.com>
2025-11-27 21:21:44 +0530

    Update Add_or_xor.cpp

--- Add_or_xor.cpp
+++ Add_or_xor.cpp
@@ -6,11 +6,12 @@ int main(){
 	while(t--){
 	int a,b,x,y;
 	cin>>a>>b>>x>>y;
-
+     ////case when a is odd and b<a-1
 	if(b<a-1 && a%2!=0){
 		cout<<-1<<endl;
 		continue;
 	}
+	//case when a is even and b<a
 	if(b<a && a%2==0){
 		cout<<-1<<endl;
 		continue;
@@ -21,10 +22,12 @@ int main(){
 		continue;
 		
 	}
+	// case when b is a-1 and a is odd
 	if(b==a-1 && a%2!=0){
 		cout<<y<<endl;
 		continue;
 	}
+	//count the cost here if a is even-> min of xor and add , if a is odd only add
 	long long cost=0;
 	while(a!=b){
 		if(a%2==0){
